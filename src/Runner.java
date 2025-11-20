d026abe2591703cd4fa0f374e2298f792374e6df
a <sample@bths.edu>
2025-11-20 10:13:35 -0500

    added a and b variables and combined those in a print statement,also changed a previous print statement slightly

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,9 @@
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("yo");
+        System.out.println("yo hi");
         System.out.println("yo whats up");
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
     }
 }
