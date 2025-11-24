0540f8637c6ae3bc4791e5b3c4ad854dc8f5b491
a <sample@bths.edu>
2025-11-24 11:04:07 -0500

    added timer and whileloopfun classes

--- src/Runner.java
+++ src/Runner.java
@@ -1,12 +1,22 @@
 public class Runner {
     public static void main(String[] args) {
-        int counter = 0;
-        while (counter >= 0) {
-            counter++;
-        }
-        System.out.println("final counter: " + counter);
+        WhileLoopFun fun = new WhileLoopFun();
 
-
+        System.out.println("-- test printDigits --");
+        fun.printDigits(5);
+        System.out.println("---");
+        fun.printDigits(361);
+        System.out.println("---");
+        fun.printDigits(98000);
+        System.out.println("---");
+        fun.printDigits(150856);
+        System.out.println("-- test countLetter --");
+        System.out.println(fun.countLetter("apple", "a"));
+        System.out.println(fun.countLetter("apple", "z"));
+        System.out.println(fun.countLetter("babababa", "a"));
+        System.out.println(fun.countLetter("babababa", "b"));
+        System.out.println(fun.countLetter("excellente", "e"));
+        System.out.println(fun.countLetter("rawrrrrrr", "r"));
 
 
     }
